d438af860c8d90aec68c28184d5a2d5e19a0cd81
saini1998 <sainiaaryaman1998@gmail.com>
2021-09-17 03:44:41 +0100

    Initial Commit

---
+++ Calnewlator/CalnewlatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalnewlatorApp.swift
+//  Calnewlator
+//
+//  Created by Aaryaman Saini on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CalnewlatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calnewlator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calnewlator
+//
+//  Created by Aaryaman Saini on 17/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
